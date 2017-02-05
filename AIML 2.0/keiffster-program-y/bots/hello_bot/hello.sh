070ec60239306414379303085ae50cc1f0182bdd
Alexander <MeisterUrian@mail.ru>
2017-02-06 00:49:17 +0900

    All anecdotes and russian cities for weather

--- AIML 2.0/keiffster-program-y/bots/hello_bot/hello.sh
+++ AIML 2.0/keiffster-program-y/bots/hello_bot/hello.sh
@@ -4,6 +4,6 @@ clear
 
 export PYTHONPATH=../../src/
 
-python3 aiml/form_weather.py
+#python3 aiml/form_weather.py
 python3 ../../src/programy/clients/console.py --config ./config.yaml --cformat yaml --logging ./logging.yaml --debug
 
